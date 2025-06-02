189b6224400a08ff5fdde768069e4c4d4b401042
akaz9579 <akaz9579@gmail.com>
2025-06-02 15:13:38 -0400

    kaggle dataset setup, formatting prep pre pandas

--- PatternData.py
+++ PatternData.py
@@ -1,4 +1,16 @@
-import kaggle
 import os
 import pandas as pd
 import json
+
+os.chdir('/Users/amankazi/CongressTradeTracker/historicData/Stocks')
+
+files = os.listdir('.') 
+
+for f in files:
+    with open('/Users/amankazi/CongressTradeTracker/historicData/Stocks/' + f , "r") as dir:
+        print(dir.read())
+
+
+
+
+
